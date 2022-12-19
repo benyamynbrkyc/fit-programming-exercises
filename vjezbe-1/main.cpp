0707f1936c80a1489bed59d9061ee52d5cebb4aa
benyamynbrkyc <benjamin@benjaminbrkic.com>
2022-12-19 14:57:10 +0100

    init

---
+++ vjezbe-1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "tasks.h"
+
+using namespace std;
+
+int main() {
+    task_19();
+
+    return 0;
+}
---
+++ vjezbe-1/tasks.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+void task_17() {
+    int n;
+    cout << "Enter a number between 10 and 99: ";
+    cin >> n;
+
+    while (!(n >= 10 && n <= 99)) {
+        cout << "Invalid input. Try again: ";
+        cin >> n;
+    }
+
+    for (int i = 10; i <= n; i++) {
+        int firstDigit = i / 10;
+        int secondDigit = i % 10;
+        if (firstDigit + secondDigit < firstDigit * secondDigit)
+            cout << i << endl;
+    }
+}
+
+void task_18() {
+    cout << "Enter number n > 10: ";
+    int n;
+    cin >> n;
+
+    while (n <= 10) {
+        cout << "Invalid input. Try again: ";
+        cin >> n;
+    }
+
+    for (int i = 10; i <= n; i++) {
+        if (i % 3 == 0 && i % 100 == 33) {
+            cout << i << " ";
+        }
+    }
+}
+
+void task_19() {
+    int day, month, year;
+    cout << "Enter date : ";
+    cin >> day >> month >> year;
+
+    // check if date and month are valid
+    if (day < 1 || day > 31 || month < 1 || month > 12) {
+        cout << "Date not valid" << endl;
+        return;
+    }
+
+    // check if year is leap year
+    bool leap_year = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+
+    if (
+        (month == 2 && day > 29) || (month == 2 && day == 29 && !leap_year) ||
+        (month == 4 && day > 30) || (month == 6 && day > 30) || (month == 8 && day > 30) || (month == 9 && day > 30) || (month == 11 && day > 30)) {
+        cout << "Date not valid" << endl;
+        return;
+    }
+
+    cout << "Date valid" << endl;
+}
